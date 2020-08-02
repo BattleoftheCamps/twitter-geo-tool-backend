fa493ada3c16fc48578f5d56f81153c4c5f64854
Ava DeLaCruz <avadelac@usc.edu>
2020-08-01 23:06:46 -0400

    add basic api

---
+++ app.py
@@ -0,0 +1,16 @@
+# import os
+from flask import Flask, jsonify, request, render_template
+# template_dir = os.path.abspath('./src')
+app = Flask(__name__, template_folder='../Front-End')
+
+# @app.route('/')
+# def render():
+# 	# serve index template
+# 	return render_template('index.html')
+
+@app.route("/helloworld")
+def output():
+	return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
