59a75fd41661be2a8eac7b90630a6c5a798e9457
Ava DeLaCruz <avadelac@usc.edu>
2020-08-01 23:23:32 -0400

    pls work heroku

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ app = Flask(__name__, template_folder='../Front-End')
 def output():
 	return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
