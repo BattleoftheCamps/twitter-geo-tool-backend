0ef9ae9b4cc75774b5aaf7b33c6460e299514738
Disha Srivastava <disha.srivastava07@gmail.com>
2020-08-26 08:12:23 -0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -2,11 +2,7 @@
 import tweepy
 from geopy import Nominatim
 
-# Create Credential Keys
-consumer_key = 'V6XKhulVp7YYY3jVJAE5yZHcR'
-consumer_secret = 'k6ydihnwe2Oue1ITswLVTSHrD5YpLQyNIQlWrxlLnvBYwjRFuL'
-access_token = '2620473211-yUybtaw3CDNd9vyHh9qSvgOul1pkiOuB0wpVEts'
-access_token_secret = 'srxKoqEdF0SJbBnB8K2mgcz1m7AOJLUVHofIB5yyycXAZ'
+
 
 # Set Up Configuration
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
